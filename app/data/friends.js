489024ab68b3a5a24d56ae9f72c1a20cf36b4202
Rich D <richie.donovan62@gmail.com>
2018-12-21 16:03:42 -0600

    Added server side logic, everything works

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,22 +3,22 @@
 var friends = [ 
     {
     name: "Jim",
-    photo: "",
-    scores: [1,2,2,1,2,1,2,2,1]
+    photo: "http://comedyunderground.com/wp-content/uploads/eric-scwartz-350x350.jpg",
+    scores: [1,2,2,1,2,1,2,2,1,2]
     },
     {
     name: "Bob",
-    photo: "",
+    photo: "https://www.excillum.com/wp-content/uploads/2018/08/E1807Bweb-fix-350x350.jpg",
     scores: [2,3,4,2,3,2,3,2,3,4]
     },
     {
     name: "Chris",
-    photo: "",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCpPnUS2uJBeTfIJw1wvAHhEWNygdnehJljr65GUnZtnkwpIsN",
     scores: [1,2,3,2,4,3,4,5,4,2]
     },
     {
     name: "Tonya",
-    photo: "",
+    photo: "http://dwellrealtypdx.com/wp-content/uploads/2017/06/Janikka-Byman-350x350.jpg",
     scores: [4,5,4,4,5,4,4,3,5,4]
     },
 ]
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -11,37 +11,38 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
 
-        console.log(newFriend);
-
-        var difference = 40;
         var matchName = "";
         var matchPhoto = "";
         var newFriend = req.body;
-    
+
+        var totalDifference;
+        var bestDifference = 50;
         console.log(newFriend);
        
         friendsData.forEach(function(friends){
-            var matchedScoresArr = [];
-            var totalDifference = 40
+            totalDifference = 0
+                
+            for (var j=0; j<newFriend.scores.length; j++){
+                totalDifference += Math.abs((parseInt(newFriend.scores[j])) - parseInt(friends.scores[j])) 
+                
+            }   
 
-            for (var i=0; i<friends.scores.length; i++) {
-                for (var j=0; j<newFriend.scores.length; j++){
-                    matchedScoresArr.push(Math.abs(parseInt(newFriend.scores[j]))- parseInt(friends.scores[i])) 
-                }
-            }
-
-            function getSum(total, num){
-                return total + num;
-            }
-
-            totalDifference = matchedScoresArr.reduce(getSum());
-
-            if (totalDifference < difference) {
-                difference = totalDifference;
+            if (totalDifference <= bestDifference) {
                 matchName = friends.name;
-                matchPhoto = friends.photo
+                matchPhoto = friends.photo;
+                bestDifference = totalDifference;
             }
+
+            // ?? Tried it with the if statement on the outside of the for loop and that doesn't print anything to the console for an answer...
+            // ?? How do I get the program to figure out which is closest? It jsut prints Tanya out every time...
+            
+
+            
         });
+
+        // ?? How do I push my answer, whatever it is, to my modal?
+
+       
 
         res.json({
             name: matchName,
@@ -51,6 +52,130 @@ module.exports = function(app) {
         friendsData.push(newFriend);
         
     });
-    
-}
 
+    // app.post("/api/friends", function(req, res) {
+
+    //     var matchName = "";
+    //     var matchPhoto = "";
+    //     var newFriend = req.body;
+    
+    //     console.log(newFriend);
+       
+    //     friendsData.forEach(function(friends){
+    //         var totalDifference = 0
+                
+    //         for (var j=0; j<newFriend.scores.length; j++){
+    //             totalDifference = Math.abs((parseInt(newFriend.scores[j])) - parseInt(friends.scores[j])); 
+    //         }   
+
+    //         console.log(totalDifference, friends.name);
+
+    //         if (totalDifference <= newFriend.scores) {
+    //             matchName = friends.name;
+    //             matchPhoto = friends.photo
+    //         }
+            
+    //     });
+
+    //     var a = res.json({
+    //         name: matchName,
+    //         photo: matchPhoto
+    //     });
+
+    //     console.log(a.name, a.photo);
+    
+    //     friendsData.push(newFriend);
+        
+    // });
+    
+// }
+
+
+// var path = require("path");
+
+// var friendsData = require("../app/data/friends");
+
+// module.exports = function(app) {
+
+//     app.get("/api/friends", function(req, res){
+//         return res.json(friendsData)
+//     })
+
+//     app.post("/api/friends", function(req, res) {
+
+//         //var difference = 40;
+//         var matchName = "";
+//         var matchPhoto = "";
+//         var newFriend = req.body;
+    
+//         console.log(newFriend);
+       
+//         friendsData.forEach(function(friends){
+//             var totalDifference = 0
+                
+//             for (var j=0; j<newFriend.scores.length; j++){
+//                 var diff = Math.abs((parseInt(newFriend.scores[j])) - parseInt(friends.scores[j])); 
+//                 totalDifference += diff;
+//                 // console.log(friends.scores[j])
+//             }
+            
+//             console.log(totalDifference, friends.name)
+
+//             if (totalDifference <= friends.scores[j]) {
+//                 matchName = friends.name;
+//                 matchPhoto = friends.photo
+//             }
+            
+//         });
+
+//         res.json({
+//             name: matchName,
+//             photo: matchPhoto
+//         });
+    
+//         friendsData.push(newFriend);
+        
+//     });
+    
+// }
+
+
+
+
+// app.post("/api/friends", function(req, res) {
+
+//     //var difference = 40;
+//     var matchName = "";
+//     var matchPhoto = "";
+//     var newFriend = req.body;
+
+//     console.log(newFriend);
+   
+//     friendsData.forEach(function(friends){
+//         var totalDifference = 0
+            
+//         for (var j=0; j<newFriend.scores.length; j++){
+//             var diff = Math.abs((parseInt(newFriend.scores[j])) - parseInt(friends.scores[j]));
+//             totalDifference += diff;
+//             if (totalDifference <= friends.scores[j]) {
+//                 matchName = friends.name;
+//                 matchPhoto = friends.photo
+//             }
+//         }   
+//         console.log(totalDifference, friends.name);
+//         console.log(diff, friends.name);
+
+        
+        
+//     });
+
+//     res.json({
+//         name: matchName,
+//         photo: matchPhoto
+//     });
+
+//     friendsData.push(newFriend);
+    
+// });
+
+}
